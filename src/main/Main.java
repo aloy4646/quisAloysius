afd4d9f4628c340a46df69035e15eee26fb55600
Aloysius <aloysiuslasroha@gmail.com>
2021-11-12 09:58:56 +0700

    Initialisasi

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import controller.Controller;
+import view.Menu;
+/**
+ *
+ * @author Aloysius
+ */
+public class Main {
+    public Main(){
+        new Menu();
+    }
+    
+    public static void main(String[] args) {
+        new Main();
+    }
+}
